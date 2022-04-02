61dc3a3a31280c01abed819aaf4d79a31bbf8a08
codehunter <hunterx6323@gmail.com>
2022-04-02 21:52:43 +0700

    add Class property descriptor info

--- src/main/java/com/example/handlingformsubmission/HandlingFormSubmissionApplication.java
+++ src/main/java/com/example/handlingformsubmission/HandlingFormSubmissionApplication.java
@@ -1,13 +1,24 @@
 package com.example.handlingformsubmission;
 
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HandlingFormSubmissionApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IntrospectionException {
 		SpringApplication.run(HandlingFormSubmissionApplication.class, args);
+
+		BeanInfo info = Introspector.getBeanInfo(Class.class);
+		PropertyDescriptor[] properties =
+				info.getPropertyDescriptors();
+		for (PropertyDescriptor pd : properties) {
+			System.out.println("Property: " + pd.getName());
+		}
 	}
 
 }
