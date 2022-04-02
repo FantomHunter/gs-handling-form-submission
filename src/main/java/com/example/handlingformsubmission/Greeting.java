4711561fd84b76cb4ae000ab7494fad554f973fa
codehunter <hunterx6323@gmail.com>
2022-04-02 17:39:24 +0700

    first commit

---
+++ src/main/java/com/example/handlingformsubmission/Greeting.java
@@ -0,0 +1,32 @@
+package com.example.handlingformsubmission;
+
+public class Greeting {
+
+	private long id;
+	private String content;
+	private Member member;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Member getMember() {
+		return member;
+	}
+
+	public void setMember(Member member) {
+		this.member = member;
+	}
+}
